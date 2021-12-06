be77e35367af160432ef5b3fef45d66ff7132442
AntonKolomiiets <anton.kolomiiets@logz.io>
2021-12-06 11:20:02 +0200

    update navigation and profile

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,15 +1,37 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+// import { useSession, getSession } from "next-auth/client";
+// import { useState, useEffect } from "react";
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+	// Redirect away if NOT auth
 
-  return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-    </section>
-  );
+	// const [isLoading, setIsLoading] = useState(true);
+	// const [loadedSession, setLoadedSession] = useState();
+	// // const [session, loading] = useSession();
+
+	// useEffect(() => {
+	// 	getSession()
+	// 		.then((session) => {
+	// 			if (!session) {
+	// 				window.location.href = "/auth";
+	// 			}
+
+	// 			setIsLoading(false);
+	// 		})
+	// 		.catch((e) => console.log(e));
+	// }, []);
+
+	// if (isLoading) {
+	// 	return <p className={classes.profile}> Loading</p>;
+	// }
+
+	return (
+		<section className={classes.profile}>
+			<h1>Your User Profile</h1>
+			<ProfileForm />
+		</section>
+	);
 }
 
 export default UserProfile;
